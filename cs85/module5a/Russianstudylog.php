9c3f1ad62ccd974e345b5ee65ee6d2b06d0d2c98
Nanda <silva_gomes_fernan01@student.smc.edu>
2026-07-09 18:51:38 -0700

    Create assignment description

---
+++ cs85/module5a/Russianstudylog.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Assignment 5A: Design a class based on your life
+ * Topic: Russian language study log
+ *
+ * I picked this topic because I've taken Russian class at SMC.
+ * This class models a daily study log: how many words I've learned,
+ * how many hours I've put in, my current level,
+ * and whether I'm hitting my own goals.
+ */
+
+ 
